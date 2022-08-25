e69b0e4e60660e33c81ea9c8328b31a27b117f62
Buzatel-Iulian <iulian.buzatel@gmail.com>
2022-08-25 22:26:41 +0300

    changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Configuration files backup
+
+openbox config may actually be in etc/xdg/openbox/ or etc/X11/openbox/
--- openbox/utils/raise_light.sh
+++ openbox/utils/raise_light.sh
@@ -1,2 +1,3 @@
 # raising the light level from the script with xrandr / arandr
 xrandr --output eDP-1 --brightness 0.8
+#                ^ Display id that you get with: xrandr -q
